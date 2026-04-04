d672bb3d0efd71cda813f3089497dee6f9a20170
arthur-georget <arthur.georget@laplateforme.io>
2026-04-05 00:41:50 +0200

    feat(jour05): job06 done.

--- jour05/jobs/src/main/java/javafx/App.java
+++ jour05/jobs/src/main/java/javafx/App.java
@@ -2,14 +2,18 @@ package javafx;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -38,6 +42,24 @@ public class App extends Application {
         });
         FlowPane flowPane = new FlowPane(20.0, 20.0,name,nameInput,sendButton);
 
+        ListView<String> listView = new ListView<String>();
+        ObservableList<String> items =FXCollections.observableArrayList (
+        "Rouge", "Bleu", "Vert", "Noir");
+        listView.setItems(items);
+        listView.setPrefHeight(70);
+
+        Button okButton = new Button("Ok");
+        okButton.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event){
+                System.out.println(listView.getSelectionModel().getSelectedItem());
+            }
+        });
+
+
+        VBox vbox = new VBox(8);
+        vbox.getChildren().addAll(flowPane, listView, okButton);
+
         Button exitButton = new Button("Quitter");
         exitButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -47,8 +69,8 @@ public class App extends Application {
         });
 
         BorderPane borderPane = new BorderPane();
-
-        borderPane.setCenter(flowPane);
+        
+        borderPane.setCenter(vbox);
         borderPane.setBottom(exitButton);
 
         scene = new Scene(borderPane, 640, 480);
