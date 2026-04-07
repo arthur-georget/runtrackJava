6accf4e1898d3e7418518743895f6917b345a000
arthur-georget <arthur.georget@laplateforme.io>
2026-04-07 09:41:21 +0200

    feat(jour05): WIP job07.

--- jour05/jobs/src/main/java/javafx/App.java
+++ jour05/jobs/src/main/java/javafx/App.java
@@ -13,11 +13,13 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-
+import java.util.ArrayList;
 import java.io.IOException;
+import java.lang.reflect.Array;
 
 /**
  * JavaFX App
@@ -29,36 +31,46 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("Mon application JavaFX du jour 05");
+        
+        ArrayList<String[]> users = new ArrayList<String[]>();
+        String[] user1 = {"Joe","100"};
+        users.add(user1);
+        Label user1Name = new Label(users.get(0)[0]);
+        Label user1Age = new Label(users.get(0)[1]);
 
+        GridPane gridpane = new GridPane();
+        GridPane.setConstraints(user1Name, 1, 0);
+        GridPane.setConstraints(user1Age, 2,0);
+
+        gridpane.getChildren().addAll(user1Name, user1Age);
 
         Label name = new Label("Nom: ");
         TextField nameInput = new TextField();
         Button sendButton = new Button("Envoyer");
-        sendButton.setOnAction(new EventHandler<ActionEvent>(){
+        sendButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event){
+            public void handle(ActionEvent event) {
                 System.out.println(nameInput.getText());
             }
         });
-        FlowPane flowPane = new FlowPane(20.0, 20.0,name,nameInput,sendButton);
+        FlowPane flowPane = new FlowPane(20.0, 20.0, name, nameInput, sendButton);
 
         ListView<String> listView = new ListView<String>();
-        ObservableList<String> items =FXCollections.observableArrayList (
-        "Rouge", "Bleu", "Vert", "Noir");
+        ObservableList<String> items = FXCollections.observableArrayList(
+                "Rouge", "Bleu", "Vert", "Noir");
         listView.setItems(items);
         listView.setPrefHeight(70);
 
         Button okButton = new Button("Ok");
-        okButton.setOnAction(new EventHandler<ActionEvent>(){
+        okButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event){
+            public void handle(ActionEvent event) {
                 System.out.println(listView.getSelectionModel().getSelectedItem());
             }
         });
 
-
         VBox vbox = new VBox(8);
-        vbox.getChildren().addAll(flowPane, listView, okButton);
+        vbox.getChildren().addAll(flowPane, listView, okButton, gridpane);
 
         Button exitButton = new Button("Quitter");
         exitButton.setOnAction(new EventHandler<ActionEvent>() {
@@ -69,7 +81,7 @@ public class App extends Application {
         });
 
         BorderPane borderPane = new BorderPane();
-        
+
         borderPane.setCenter(vbox);
         borderPane.setBottom(exitButton);
 
